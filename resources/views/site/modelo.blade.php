824b6bb5a87a6533579a1bf025721b0e2a24a72b
Sergio <sergiopeluzzi@gmail.com>
2015-10-26 14:03:41 -0400

    Link do cardapio alterado

--- resources/views/site/modelo.blade.php
+++ resources/views/site/modelo.blade.php
@@ -304,131 +304,132 @@
             </div><!-- fim div container nav -->
         </div><!-- fim div inner header-->
     </header> <!-- fim da div header -->
+    </div>
 
-@yield('body')
+    @yield('body')
 
-<footer id="footer">
-    <!-- Acessar informações gerais -->
-    <div id="inner-footer">
-        <div class="container">
-            <div class="row">
+    <footer id="footer">
+        <!-- Acessar informações gerais -->
+        <div id="inner-footer">
+            <div class="container">
+                <div class="row">
 
-                <!-- Informar acesso a minha conta -->
-                <div class="col-md-3 col-sm-4 col-xs-12 widget">
-                    <h3>DÚVIDAS</h3>
-                    <ul class="links">
-                        <li><a href="{{ url('meuspedidos') }}">Meus pedidos</a></li>
-                        <li><a href="{{ url('minhaconta') }}">Minha conta</a></li>
-                        <li><a href="{{ url('contato') }}">Sugestões</a></li>
-                    </ul>
-                </div>
+                    <!-- Informar acesso a minha conta -->
+                    <div class="col-md-3 col-sm-4 col-xs-12 widget">
+                        <h3>DÚVIDAS</h3>
+                        <ul class="links">
+                            <li><a href="{{ url('meuspedidos') }}">Meus pedidos</a></li>
+                            <li><a href="{{ url('minhaconta') }}">Minha conta</a></li>
+                            <li><a href="{{ url('contato') }}">Sugestões</a></li>
+                        </ul>
+                    </div>
 
-                <!-- Informar acesso as informações da página -->
-                <div class="col-md-3 col-sm-4 col-xs-12 widget">
-                    <h3> INFORMAÇÕES </h3>
-                    <ul class="links">
-                        <li><a href="/#festa_principal">A Festa</a></li>
-                        <li><a href="https://www.facebook.com/pages/Millenium/558733344212135?rf=234786046680210" target="_blank">Local do evento</a></li>
-                        <li><a href="/#cardapio_principal">Cardápio</a></li>
-                        <li><a href="/#patrocinadores_principal">Patrocinadores</a></li>
-                    </ul>
-                </div>
+                    <!-- Informar acesso as informações da página -->
+                    <div class="col-md-3 col-sm-4 col-xs-12 widget">
+                        <h3> INFORMAÇÕES </h3>
+                        <ul class="links">
+                            <li><a href="/#festa_principal">A Festa</a></li>
+                            <li><a href="https://www.facebook.com/pages/Millenium/558733344212135?rf=234786046680210" target="_blank">Local do evento</a></li>
+                            <li><a href="/#cardapio_principal">Cardápio</a></li>
+                            <li><a href="/#patrocinadores_principal">Patrocinadores</a></li>
+                        </ul>
+                    </div>
 
-                <!-- Informar dados sobre a organizadora do evento -->
-                <div class="col-md-3 col-sm-4 col-xs-12 widget">
-                    <h3> ORGANIZAÇÃO </h3>
-                    <ul class="contact-list">
-                        <li><strong>IGREJA EVANGÉLICA SHEKINA</strong>
-                        </li>
-                        <li>Brasil</li>
-                        <li>Rondônia</li>
-                        <li>Cacoal</li>
-                        <li>Rua da Igreja 546 - Princesa Izabel</li>
-                        <li>Aberto de: Seg. - Sex.</li>
-                        <li>Nos horários: 8.00AM - 18.00PM</li>
-                    </ul>
-                </div>
-                <div class="clearfix visible-sm"></div>
-                <div class="col-md-3 col-sm-12 col-xs-12 widget">
-                    <h3>FACEBOOK</h3>
-                    <div class="facebook-likebox">
-                        <div class="fb-like"
-                             data-href="https://www.facebook.com/pages/Igreja-Evang%C3%A9lica-Shekina/422667231144835?fref=ts" &amp;
-                             data-width="80" &amp; data-layout="standard" &amp; data-colorscheme="dark" &amp;
-                             data-action="like" &amp; data-show-faces="true" &amp; data-share="false">
+                    <!-- Informar dados sobre a organizadora do evento -->
+                    <div class="col-md-3 col-sm-4 col-xs-12 widget">
+                        <h3> ORGANIZAÇÃO </h3>
+                        <ul class="contact-list">
+                            <li><strong>IGREJA EVANGÉLICA SHEKINA</strong>
+                            </li>
+                            <li>Brasil</li>
+                            <li>Rondônia</li>
+                            <li>Cacoal</li>
+                            <li>Rua da Igreja 546 - Princesa Izabel</li>
+                            <li>Aberto de: Seg. - Sex.</li>
+                            <li>Nos horários: 8.00AM - 18.00PM</li>
+                        </ul>
+                    </div>
+                    <div class="clearfix visible-sm"></div>
+                    <div class="col-md-3 col-sm-12 col-xs-12 widget">
+                        <h3>FACEBOOK</h3>
+                        <div class="facebook-likebox">
+                            <div class="fb-like"
+                                 data-href="https://www.facebook.com/pages/Igreja-Evang%C3%A9lica-Shekina/422667231144835?fref=ts" &amp;
+                                 data-width="80" &amp; data-layout="standard" &amp; data-colorscheme="dark" &amp;
+                                 data-action="like" &amp; data-show-faces="true" &amp; data-share="false">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <!-- Informar icones rodapé -->
-    <div id="footer-bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-7 col-sm-7 col-xs-12 footer-social-links-container">
-                    <ul class="social-links clearfix">
-                        <li>
-                            <a href="https://www.facebook.com/pages/Igreja-Evang%C3%A9lica-Shekina/422667231144835?ref=br_rs"
-                               target="_blank" class="social-icon icon-facebook">
-                            </a>
-                        </li>
-                        <li><a href="http://www.skype.com/pt-br/" target="_blank" class="social-icon icon-skype"></a></li>
-                        <li><a href="mailto:igreja_shekina@gmail.com" target="_blank" class="social-icon icon-email"></a></li>
-                    </ul>
-                </div>
+        <!-- Informar icones rodapé -->
+        <div id="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7 col-sm-7 col-xs-12 footer-social-links-container">
+                        <ul class="social-links clearfix">
+                            <li>
+                                <a href="https://www.facebook.com/pages/Igreja-Evang%C3%A9lica-Shekina/422667231144835?ref=br_rs"
+                                   target="_blank" class="social-icon icon-facebook">
+                                </a>
+                            </li>
+                            <li><a href="http://www.skype.com/pt-br/" target="_blank" class="social-icon icon-skype"></a></li>
+                            <li><a href="mailto:igreja_shekina@gmail.com" target="_blank" class="social-icon icon-email"></a></li>
+                        </ul>
+                    </div>
 
-                <!-- Informar sobre empresa desenvolvedora -->
-                <div class="col-md-5 col-sm-5 col-xs-12 footer-text-container">
-                    <p>&copy; 2015 us.E Tecnologia&trade;</p>
+                    <!-- Informar sobre empresa desenvolvedora -->
+                    <div class="col-md-5 col-sm-5 col-xs-12 footer-text-container">
+                        <p>&copy; 2015 us.E Tecnologia&trade;</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</footer><!-- fim rodapé -->
+    </footer><!-- fim rodapé -->
 
-<!-- Informar elementos modal Login-->
-<div class="modal fade" id="modal-login-form" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
-     aria-hidden="true" style="display: none;">
-    <form id="login-form" method="POST" action="{{ url('/auth/login') }}">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span
-                                class="sr-only">Sair</span></button>
-                    <h4 class="modal-title" id="myModalLabel1">Entrar</h4>
-                </div>
-                <!-- Fim do modal header-->
-                <div class="modal-body">
-                    <div class="input-group">
-                            <span class="input-group-addon"><span class="input-icon input-icon-email"></span><span
-                                        class="input-text"><i class="fa fa-envelope"></i> E-mail</span></span>
-                        <input type="email" required="" class="form-control input-lg" placeholder="Seu e-mail" name="email" value="{{ old('email') }}">
+    <!-- Informar elementos modal Login-->
+    <div class="modal fade" id="modal-login-form" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1"
+         aria-hidden="true" style="display: none;">
+        <form id="login-form" method="POST" action="{{ url('/auth/login') }}">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span
+                                    class="sr-only">Sair</span></button>
+                        <h4 class="modal-title" id="myModalLabel1">Entrar</h4>
                     </div>
-                    <!-- Fim  do input group-->
-                    <div class="input-group">
-                            <span class="input-group-addon"><span class="input-icon input-icon-password"></span><span
-                                        class="input-text"><i class="fa fa-lock fa-2x"></i> Senha</span></span>
-                        <input type="password" required="" class="form-control input-lg" placeholder="Sua senha" name="password">
-                    </div>
-                    <!-- Fim do input group -->
+                    <!-- Fim do modal header-->
+                    <div class="modal-body">
+                        <div class="input-group">
+                                <span class="input-group-addon"><span class="input-icon input-icon-email"></span><span
+                                            class="input-text"><i class="fa fa-envelope"></i> E-mail</span></span>
+                            <input type="email" required="" class="form-control input-lg" placeholder="Seu e-mail" name="email" value="{{ old('email') }}">
+                        </div>
+                        <!-- Fim  do input group-->
+                        <div class="input-group">
+                                <span class="input-group-addon"><span class="input-icon input-icon-password"></span><span
+                                            class="input-text"><i class="fa fa-lock fa-2x"></i> Senha</span></span>
+                            <input type="password" required="" class="form-control input-lg" placeholder="Sua senha" name="password">
+                        </div>
+                        <!-- Fim do input group -->
 
-                    <span class="help-block text-right"><a href="#">Esqueceu sua senha?</a></span>
+                        <span class="help-block text-right"><a href="#">Esqueceu sua senha?</a></span>
 
-                    <div class="input-group custom-checkbox sm-margin top-10px">
-                        <input type="checkbox" name="remember"><span class="checbox-container"><i class="fa fa-check"></i></span>Lembrar-me
-                    </div>
-                </div><!-- Fim do .modal body -->
-                <div class="modal-footer">
-                    <button class="btn btn-custom-2">ENVIAR</button>
-                    <button type="button" class="btn btn-custom" data-dismiss="modal">CANCELAR</button>
-                 </div> <!-- Fim do .modal-footer -->
-            </div><!-- Fim do .modal-content -->
-        </div><!-- Fim do .modal-dialog -->
-    </form> <!-- fim form modal login -->
-</div> <!-- fim div modal -->
+                        <div class="input-group custom-checkbox sm-margin top-10px">
+                            <input type="checkbox" name="remember"><span class="checbox-container"><i class="fa fa-check"></i></span>Lembrar-me
+                        </div>
+                    </div><!-- Fim do .modal body -->
+                    <div class="modal-footer">
+                        <button class="btn btn-custom-2">ENVIAR</button>
+                        <button type="button" class="btn btn-custom" data-dismiss="modal">CANCELAR</button>
+                     </div> <!-- Fim do .modal-footer -->
+                </div><!-- Fim do .modal-content -->
+            </div><!-- Fim do .modal-dialog -->
+        </form> <!-- fim form modal login -->
+    </div> <!-- fim div modal -->
 </body>
 <footer>
     <!-- Importar facebook organizador -->
@@ -441,7 +442,7 @@
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
 
-    </div> <!-- fim div conteudo -->
+
 
     <!-- script login -->
     <script>
