2fda7c4e4c1eda126682aa2e9720c59c68a527ec
Sergio <sergiopeluzzi@gmail.com>
2015-11-11 17:43:34 -0400

    Ajustes Emails

--- resources/views/site/sucesso.blade.php
+++ resources/views/site/sucesso.blade.php
@@ -7,7 +7,7 @@
         <img src="{{ asset('site/images/homeslider/Success_Icon_256.png') }}"/>
         <h1>PEDIDO REALIZADO COM SUCESSO!!!</h1>
         <h3>Seu pedido foi registrado e aguarda confirmação de pagamento: Cod. verificador: {{ $transacao }}</h3>
-        <h4>O status mudará automaticamente assim que o PagSeguro no enviar a confirmação de pagamento</h4>
+        <h4>O status mudará automaticamente assim que o PagSeguro nos enviar a confirmação de pagamento</h4>
         <div class="col-sm-4 col-lg-offset-4">
             <a href="/meuspedidos" class="form-control btn btn-custom-2 btn-danger">Clique aqui para ver seus pedidos</a>
 
