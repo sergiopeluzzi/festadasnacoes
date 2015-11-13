4a2dea92bd467548c3f0de8cb2ce4ddb2a55b6f0
Sergio <sergiopeluzzi@gmail.com>
2015-11-13 09:49:57 -0400

    insert data e hora banner principal

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -17,7 +17,7 @@
                          data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
                     <div class="tp-caption rev-title lfr ltr" data-x="495" data-y="100" data-speed="1200"
                          data-start="400" data-endspeed="200">
-                        21.11.2015 à partir das 19:00hs - Millenium - Cacoal/RO
+                        <font color="red">21.11.2015 à partir das 19:00hs - Millenium - Cacoal/RO</font>
                     </div>
                     <div class="tp-caption rev-title lfr ltr" data-x="695" data-y="200" data-speed="1600"
                          data-start="300" data-endspeed="300">
